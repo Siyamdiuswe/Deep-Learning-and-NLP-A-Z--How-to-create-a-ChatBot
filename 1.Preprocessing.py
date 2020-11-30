885b2253dfb6a1c1314961716cc053ab8ef55bcb
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-12-01 03:17:04 +0600

    clean text

--- 1.Preprocessing.py
+++ 1.Preprocessing.py
@@ -29,3 +29,22 @@ for conversation in conversations_ids:
     for i in range(len(conversation)-1):
         questions.append(id2line[conversation[i]])
         answers.append(id2line[conversation[i+1]])
+
+#Doing a first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"didn't", "did not", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"[-()\"#/@;:<>+-=~|.,?]", "", text)
+    return text
