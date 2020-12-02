f6b3ceeca31c6ac232fcafed5bc35cd2229614ff
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-12-02 17:00:48 +0600

    complete

--- 1.Preprocessing.py
+++ 1.Preprocessing.py
@@ -10,58 +10,76 @@ import time
 lines = open('C:/Users/Siyam/Desktop/github/Deep-Learning-and-NLP-A-Z--How-to-create-a-ChatBot/movie_lines.txt').read().split('\n')
 conversations = open('C:/Users/Siyam/Desktop/github/Deep-Learning-and-NLP-A-Z--How-to-create-a-ChatBot/movie_conversations.txt').read().split('\n')
 
-#Creating a library that maps each line and it's id
+# Creating a dictionary that maps each line and its id
 id2line = {}
-for line in lines[:-1]:  #As last row is empty
+for line in lines:
     _line = line.split(' +++$+++ ')
-    id2line[_line[0]] = _line[-1]
-
-# List of conversations
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+ 
+# Creating a list of all of the conversations
 conversations_ids = []
 for conversation in conversations[:-1]:
-    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "") # Splitting the conversations, and formatting them to remove [];'';spaces
-    conversations_ids.append(_conversation.split(',')) # Formating the large list to consist only the id's in a list
-    
-#Getting separately the questions and answers
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+ 
+# Getting separately the questions and the answers
 questions = []
 answers = []
 for conversation in conversations_ids:
-    for i in range(len(conversation)-1):
+    for i in range(len(conversation) - 1):
         questions.append(id2line[conversation[i]])
         answers.append(id2line[conversation[i+1]])
-
-# Cleaning the text function
+ 
+# Doing a first cleaning of the texts
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"i'm", "i am", text)
     text = re.sub(r"he's", "he is", text)
     text = re.sub(r"she's", "she is", text)
-    text = re.sub(r"don't", "do not", text)
-    text = re.sub(r"can't", "can not", text)
-    text = re.sub(r"didn't", "did not", text)
     text = re.sub(r"that's", "that is", text)
     text = re.sub(r"what's", "what is", text)
     text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
     text = re.sub(r"\'ll", " will", text)
     text = re.sub(r"\'ve", " have", text)
     text = re.sub(r"\'re", " are", text)
     text = re.sub(r"\'d", " would", text)
-    text = re.sub(r"[-()\"#/@;:<>+-=~|.,?]", "", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
     return text
-
-
-# Cleaning Questions
+ 
+# Cleaning the questions
 clean_questions = []
-for q in questions:
-    clean_questions.append(clean_text(q))
-
-
-# Cleaning Answers
+for question in questions:
+    clean_questions.append(clean_text(question))
+ 
+# Cleaning the answers
 clean_answers = []
-for a in answers:
-    clean_answers.append(clean_text(a))
-    
-#Creating a dictionary that maps each words  to its number of occurances
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+ 
+# Filtering out the questions and answers that are too short or too long
+short_questions = []
+short_answers = []
+i = 0
+for question in clean_questions:
+    if 2 <= len(question.split()) <= 25:
+        short_questions.append(question)
+        short_answers.append(clean_answers[i])
+    i += 1
+clean_questions = []
+clean_answers = []
+i = 0
+for answer in short_answers:
+    if 2 <= len(answer.split()) <= 25:
+        clean_answers.append(answer)
+        clean_questions.append(short_questions[i])
+    i += 1
+ 
+# Creating a dictionary that maps each word to its number of occurrences
 word2count = {}
 for question in clean_questions:
     for word in question.split():
@@ -69,48 +87,72 @@ for question in clean_questions:
             word2count[word] = 1
         else:
             word2count[word] += 1
-
 for answer in clean_answers:
     for word in answer.split():
         if word not in word2count:
             word2count[word] = 1
         else:
             word2count[word] += 1
-            
-#Creating two dictionaries that map the questions words and the answers words
-threshold = 20
-questionsword2int = {}
-word_number = 0
-for word, count in word2count.items():
-     if count >= threshold:
-         questionsword2int[word] = word_number
-         word_number += 1
-
-answersword2int = {}
-word_number = 0
-for word, count in word2count.items():
-     if count >= threshold:
-         answersword2int[word] = word_number
-         word_number += 1
-            
-#Adding the last tokens to these two dictionaries
-tokens = ['<PAD>','<EOS>','<OUT>','<SOS>']
-for token in tokens:
-    questionsword2int[token]=len(questionsword2int)+1
-
-for token in tokens:
-    answersword2int[token] = len(answersword2int)+1
  
-# Creating an inverse dictionary of answersword2int
-answersints2word = {w_i:w for w,w_i in answersword2int.items()}
-
-# Adding EOS after the answer
+# Creating two dictionaries that map the questions words and the answers words to a unique integer
+threshold_questions = 15
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold_questions:
+        questionswords2int[word] = word_number
+        word_number += 1
+threshold_answers = 15
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold_answers:
+        answerswords2int[word] = word_number
+        word_number += 1
+ 
+# Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+ 
+# Creating the inverse dictionary of the answerswords2int dictionary
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+ 
+# Adding the End Of String token to the end of every answer
 for i in range(len(clean_answers)):
-    clean_answers[i] += ' <EOS>'    
+    clean_answers[i] += ' <EOS>'
  
+# Translating all the questions and the answers into integers
+# and Replacing all the words that were filtered out by <OUT> 
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_into_int.append(ints)
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+    answers_into_int.append(ints)
     
+# Sorting questions and answers by the length of questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
  
-    
- 
-    
 print("OK!")
+print(questionswords2int['<OUT>'])
